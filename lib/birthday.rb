bb0c920ab73d40d0454b7e9a42a0922b011bb4da
Sarah Glotzbach <sarah.glotzbach@gmail.com>
2019-10-11 03:59:03 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,9 @@ def happy_birthday(birthday_kids)
   birthday_kids.each{ |name,age|
     if age<12
     puts"Happy Birthday #{name}! You are now #{age} years old!"
-  end
+    else
+      puts "You are now too old for this."
+    end
   }
 end
 
